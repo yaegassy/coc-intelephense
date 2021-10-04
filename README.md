84b6f5e38c47f48cf87e88cfbcc377c784535af0
yaegassy <yaegassy@users.noreply.github.com>
2021-10-04 09:49:20 +0900

    feat: add snippets completion feature via CompletionItemProvider

--- README.md
+++ README.md
@@ -27,6 +27,8 @@ Plug 'yaegassy/coc-intelephense', {'do': 'yarn install --frozen-lockfile'}
 - `intelephense.enable`: Enable coc-intelephense extension, default `true`
 - `intelephense.path`: Absolute path to intelephense module. If there is no setting, the built-in module will be used. e.g. `/path/to/node_modules/intelephense`. default: ""
 - `intelephense.server.disableCompletion`: Disable completion only (server), default: `false`
+- `intelephense.client.disableSnippetsCompletion`: Disable snippets completion only (client), default: `false`
+- `intelephense.client.snippetsCompletionExclude`: Exclude specific prefix in snippet completion, e.g. `["class", "fun"]`, default: `[]`
 - `intelephense.progress.enable`: Enable progress window for indexing, If false, display with echo messages, default: `true` [DEMO](https://github.com/yaegassy/coc-intelephense/pull/2)
 
 **Same configuration as vscode-intelephense**:
---
+++ src/completion/IntelephenseSnippetsCompletion.ts
@@ -0,0 +1,101 @@
+import {
+  CancellationToken,
+  CompletionContext,
+  CompletionItem,
+  CompletionItemKind,
+  CompletionItemProvider,
+  CompletionList,
+  ExtensionContext,
+  InsertTextFormat,
+  Position,
+  TextDocument,
+  workspace,
+} from 'coc.nvim';
+
+import path from 'path';
+import fs from 'fs';
+
+type SnippetsJsonType = {
+  [key: string]: {
+    description: string;
+    prefix: string;
+    body: string | string[];
+  };
+};
+
+export class IntelephenseSnippetsCompletionProvider implements CompletionItemProvider {
+  private _context: ExtensionContext;
+  private snippetsFilePath: string;
+  private excludeSnippetsKeys: string[];
+
+  constructor(context: ExtensionContext) {
+    this._context = context;
+    this.snippetsFilePath = path.join(this._context.extensionPath, 'snippets', 'php.code-snippets');
+    this.excludeSnippetsKeys = workspace
+      .getConfiguration('intelephense')
+      .get<string[]>('client.snippetsCompletionExclude', []);
+  }
+
+  async getSnippetsCompletionItems(snippetsFilePath: string) {
+    const snippetsCompletionList: CompletionItem[] = [];
+    if (fs.existsSync(snippetsFilePath)) {
+      const snippetsJsonText = fs.readFileSync(snippetsFilePath, 'utf8');
+      const snippetsJson: SnippetsJsonType = JSON.parse(snippetsJsonText);
+      if (snippetsJson) {
+        Object.keys(snippetsJson).map((key) => {
+          // Check exclude
+          if (this.excludeSnippetsKeys.includes(snippetsJson[key].prefix)) return;
+
+          let snippetsText: string;
+          const body = snippetsJson[key].body;
+          if (body instanceof Array) {
+            snippetsText = body.join('\n');
+          } else {
+            snippetsText = body;
+          }
+
+          // In this extention, "insertText" is handled by "resolveCompletionItem".
+          // In "provideCompletionItems", if "insertText" contains only snippets data,
+          // it will be empty when the candidate is selected.
+          snippetsCompletionList.push({
+            label: snippetsJson[key].prefix,
+            kind: CompletionItemKind.Snippet,
+            filterText: snippetsJson[key].prefix,
+            detail: snippetsJson[key].description,
+            documentation: snippetsText,
+            insertTextFormat: InsertTextFormat.Snippet,
+            // The "snippetsText" that will eventually be added to the insertText
+            // will be stored in the "data" key
+            data: snippetsText,
+          });
+        });
+      }
+    }
+
+    return snippetsCompletionList;
+  }
+
+  async provideCompletionItems(
+    document: TextDocument,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    position: Position,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    token: CancellationToken,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    context: CompletionContext
+  ): Promise<CompletionItem[] | CompletionList> {
+    const doc = workspace.getDocument(document.uri);
+    if (!doc) return [];
+
+    const completionList = this.getSnippetsCompletionItems(this.snippetsFilePath);
+
+    return completionList;
+  }
+
+  async resolveCompletionItem(item: CompletionItem): Promise<CompletionItem> {
+    if (item.kind === CompletionItemKind.Snippet) {
+      item.insertText = item.data;
+    }
+    return item;
+  }
+}
