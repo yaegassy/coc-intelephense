c74eb14f6017612c9747bcc6b03a328c698c70ec
yaegassy <yaegassy@users.noreply.github.com>
2022-03-14 01:10:56 +0900

    feat: add client-base "codelens" and "commands" using phpunit

---
+++ src/parsers.ts
@@ -0,0 +1,125 @@
+import { LinesTextDocument, Position } from 'coc.nvim';
+import { Comment, Engine, Node } from 'php-parser';
+const parserEngine = new Engine({
+  parser: {
+    extractDoc: true,
+    php7: true,
+    locations: true,
+    suppressErrors: true,
+  },
+  ast: {
+    all_tokens: true,
+    withPositions: true,
+  },
+});
+
+type MethodType = {
+  name: string;
+  startLine: number;
+  endLine: number;
+  comments: string[];
+};
+
+export async function getMethods(document: LinesTextDocument) {
+  const code = document.getText();
+
+  const methods: MethodType[] = [];
+
+  try {
+    const parsed = parserEngine.parseEval(code.replace('<?php', '').replace('?>', ''));
+
+    parsed.children.forEach((node) => {
+      if ('children' in node) {
+        const subNode = node['children'] as Node[];
+        subNode.forEach((node) => {
+          if (node.kind === 'class') {
+            if ('body' in node) {
+              const subNode = node['body'] as Node[];
+              subNode.forEach((node) => {
+                if (node.kind === 'method') {
+                  if ('loc' in node) {
+                    const name = node['name']['name'] as string;
+                    const startLine = node['loc'] ? node['loc']['start']['line'] : 0;
+                    const endLine = node['loc'] ? node['loc']['end']['line'] : 0;
+                    const comments: string[] = [];
+                    if ('leadingComments' in node) {
+                      const leadingComments = node['leadingComments'] as Comment[];
+                      leadingComments.forEach((n) => {
+                        comments.push(n.value);
+                      });
+                    }
+
+                    const methodData: MethodType = {
+                      name,
+                      startLine,
+                      endLine,
+                      comments,
+                    };
+
+                    methods.push(methodData);
+                  }
+                }
+              });
+            }
+          }
+        });
+      }
+    });
+  } catch (e) {
+    // noop
+  }
+
+  return methods;
+}
+
+export function getTestMethods(methods: MethodType[]) {
+  const testMethods: MethodType[] = [];
+
+  methods.forEach((m) => {
+    let exists = false;
+    if (m.name) {
+      if (m.name.startsWith('test')) {
+        exists = true;
+      }
+    }
+
+    if (m.comments) {
+      m.comments.forEach((c) => {
+        if (c.includes('@test')) {
+          exists = true;
+        }
+      });
+    }
+
+    if (exists) {
+      testMethods.push(m);
+    }
+  });
+
+  return testMethods;
+}
+
+export function getTestName(methods: MethodType[], position: Position) {
+  let testName = '';
+
+  methods.forEach((m) => {
+    if (position.line + 1 >= m.startLine && position.line + 1 <= m.endLine) {
+      const name = m.name;
+
+      let isTestAnotation = false;
+      if (m.comments) {
+        m.comments.forEach((c) => {
+          if (c.includes('@test')) {
+            isTestAnotation = true;
+          }
+        });
+      }
+
+      if (name.startsWith('test') || isTestAnotation) {
+        testName = name;
+      }
+    }
+  });
+
+  return testName;
+}
