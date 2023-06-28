13153b08d5c9d81439fe98b7baf1953d28a027b4
yaegassy <yaegassy@users.noreply.github.com>
2023-06-28 10:57:30 +0900

    chore(ci): add snippets-check

---
+++ scripts/upstream-snippets-check.mjs
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+
+const FEATCH_URL =
+  'https://raw.githubusercontent.com/microsoft/vscode/master/extensions/php/snippets/php.code-snippets';
+const RESOURCE_FILE_PATH = new URL(`../data/snippets/php.code-snippets`, import.meta.url).pathname;
+
+await (async () => {
+  const res = await fetch(FEATCH_URL);
+  if (!res.ok) return;
+  const remoteData = await res.text();
+
+  const localData = await fs.promises.readFile(RESOURCE_FILE_PATH, { encoding: 'utf-8' });
+
+  if (remoteData === localData) {
+    console.log('OK');
+    process.exit(0);
+  } else {
+    console.log('NG');
+    process.exit(1);
+  }
+})();
